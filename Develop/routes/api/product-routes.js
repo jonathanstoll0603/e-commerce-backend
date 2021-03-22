4c4ba1aa693f0c627a6c88d9212e4dd72a5d2a6a
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-03-22 17:07:44 -0400

    fixed bug with tag_id

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -35,17 +35,18 @@ router.get('/:id', async (req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+        "product_name": "pool floaty",
+        "price": 15.00,
+        "stock": 10,
+        "category_id": 2,
+        "tag_id": [3, 4]
     }
   */
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
+      if (req.body.tag_id.length) {
+        const productTagIdArr = req.body.tag_id.map((tag_id) => {
           return {
             product_id: product.id,
             tag_id,
@@ -56,7 +57,7 @@ router.post('/', (req, res) => {
       // if no product tags, just respond
       res.status(200).json(product);
     })
-    .then((productTagIds) => res.status(200).json(productTagIds))
+    .then((productTagIDs) => res.status(200).json(productTagIDs))
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
@@ -77,10 +78,10 @@ router.put('/:id', (req, res) => {
     })
     .then((productTags) => {
       // get list of current tag_ids
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      const producttag_id = productTags.map(({ tag_id }) => tag_id);
       // create filtered list of new tag_ids
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
+      const newProductTags = req.body.tag_id
+        .filter((tag_id) => !producttag_id.includes(tag_id))
         .map((tag_id) => {
           return {
             product_id: req.params.id,
@@ -89,7 +90,7 @@ router.put('/:id', (req, res) => {
         });
       // figure out which ones to remove
       const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .filter(({ tag_id }) => !req.body.tag_id.includes(tag_id))
         .map(({ id }) => id);
 
       // run both actions
